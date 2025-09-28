9b18765912ebf7a6493eddbce2192f796fc1fb2a
Norbert Gulácsi <Norbert_Gulacsi@epam.com>
2025-09-28 18:42:39 +0200

    module1 - image model added

---
+++ src/main/java/com/example/demo/model/Image.java
@@ -0,0 +1,70 @@
+package com.example.demo.model;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.UUID;
+
+import com.example.demo.annotations.TableName;
+
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+
+@DynamoDbBean
+@TableName(name = "images")
+public class Image {
+  private UUID id;
+  private String objectPath;
+  private String objectSize;
+  private LocalDateTime timeAdded;
+  private LocalDateTime timeUpdated;
+  private Set<String> labels;
+
+  @DynamoDbPartitionKey
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public String getObjectPath() {
+    return objectPath;
+  }
+
+  public void setObjectPath(String objectPath) {
+    this.objectPath = objectPath;
+  }
+
+  public String getObjectSize() {
+    return objectSize;
+  }
+
+  public void setObjectSize(String objectSize) {
+    this.objectSize = objectSize;
+  }
+
+  public LocalDateTime getTimeAdded() {
+    return timeAdded;
+  }
+
+  public void setTimeAdded(LocalDateTime timeAdded) {
+    this.timeAdded = timeAdded;
+  }
+
+  public LocalDateTime getTimeUpdated() {
+    return timeUpdated;
+  }
+
+  public void setTimeUpdated(LocalDateTime timeUpdated) {
+    this.timeUpdated = timeUpdated;
+  }
+
+  public Set<String> getLabels() {
+    return labels;
+  }
+
+  public void setLabels(Set<String> labels) {
+    this.labels = labels;
+  }
+}
