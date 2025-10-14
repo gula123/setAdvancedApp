58e33a08874055cafcd1258ac9127d536fed3646
Norbert Gulácsi <Norbert_Gulacsi@epam.com>
2025-10-14 22:26:42 +0200

    module1 - updating with lombok based on pr comment

--- src/main/java/com/example/demo/model/Image.java
+++ src/main/java/com/example/demo/model/Image.java
@@ -1,81 +1,38 @@
 package com.example.demo.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
 import java.time.LocalDateTime;
 import java.util.Set;
 
 import com.example.demo.annotations.TableName;
-
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSortKey;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @DynamoDbBean
 @TableName(propertyName = "app.dynamodb.image-table-name")
 public class Image {
-  private String id;
-  private String objectPath;
-  private String objectSize;
-  private LocalDateTime timeAdded;
-  private LocalDateTime timeUpdated;
-  private Set<String> labels;
+    private String id;
+    private String objectPath;
+    private String objectSize;
+    private LocalDateTime timeAdded;
+    private LocalDateTime timeUpdated;
+    private Set<String> labels;
+    private Status status;
 
-  private Status status;
+    @DynamoDbPartitionKey
+    public String getId() {
+        return id;
+    }
 
-  public Status getStatus() {
-    return status;
-  }
-
-  public void setStatus(Status status) {
-    this.status = status;
-  }
-
-  @DynamoDbPartitionKey
-  public String getId() {
-    return id;
-  }
-
-  public void setId(String id) {
-    this.id = id;
-  }
-
-  @DynamoDbSortKey
-  public String getObjectPath() {
-    return objectPath;
-  }
-
-  public void setObjectPath(String objectPath) {
-    this.objectPath = objectPath;
-  }
-
-  public String getObjectSize() {
-    return objectSize;
-  }
-
-  public void setObjectSize(String objectSize) {
-    this.objectSize = objectSize;
-  }
-
-  public LocalDateTime getTimeAdded() {
-    return timeAdded;
-  }
-
-  public void setTimeAdded(LocalDateTime timeAdded) {
-    this.timeAdded = timeAdded;
-  }
-
-  public LocalDateTime getTimeUpdated() {
-    return timeUpdated;
-  }
-
-  public void setTimeUpdated(LocalDateTime timeUpdated) {
-    this.timeUpdated = timeUpdated;
-  }
-
-  public Set<String> getLabels() {
-    return labels;
-  }
-
-  public void setLabels(Set<String> labels) {
-    this.labels = labels;
-  }
+    @DynamoDbSortKey
+    public String getObjectPath() {
+        return objectPath;
+    }
 }
